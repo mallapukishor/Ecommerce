f1ca027fa7408ab5800f6eb353e563ecc2ecae18
Your Name <mallapukishor143@gmail.com>
2026-04-06 05:42:41 +0530

    commit

---
+++ src/main/java/com/kishor/Ecommerce/project/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.kishor.Ecommerce.project.security.services;
+
+import com.kishor.Ecommerce.project.model.User;
+import com.kishor.Ecommerce.project.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+
+
+}
